9953ba009502bfa28a80cacc09ac59ff5b7c8c6e
Mahabat <mahabatka@gmail.com>
2025-07-08 09:23:25 +0600

    version 1.01

--- src/main/java/com/autobots/ResetDemo.java
+++ src/main/java/com/autobots/ResetDemo.java
@@ -3,5 +3,6 @@ package com.autobots;
 public class ResetDemo {
     public static void main(String[] args) {
         System.out.println("Reset demo 1.00");
+        System.out.println("Reset demo 1.01");
     }
 }
