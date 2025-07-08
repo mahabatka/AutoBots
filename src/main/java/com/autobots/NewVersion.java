2f2412ce8ae8b9730e9ffe7a76d283519e5c8f15
Mahabat <mahabatka@gmail.com>
2025-07-08 09:22:11 +0600

    version 1.00

---
+++ src/main/java/com/autobots/NewVersion.java
@@ -0,0 +1,7 @@
+package com.autobots;
+
+public class NewVersion {
+    public static void main(String[] args) {
+        System.out.println("Version 4.00");
+    }
+}
---
+++ src/main/java/com/autobots/ResetDemo.java
@@ -0,0 +1,7 @@
+package com.autobots;
+
+public class ResetDemo {
+    public static void main(String[] args) {
+        System.out.println("Reset demo 1.00");
+    }
+}
